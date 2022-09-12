277d8d57baca99403c421c81d4a8602b4442db61
Isabelli Gomes <isabelli.pgomes@poli.ufrj.br>
2022-09-12 10:31:41 -0300

    Projeto 3 - Estrutura visual: Tabela de transações, busca e modal

--- Trilha ReactJS Atualizada/Projeto 3/03-dt-money/src/components/Header/index.tsx
+++ Trilha ReactJS Atualizada/Projeto 3/03-dt-money/src/components/Header/index.tsx
@@ -1,13 +1,22 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
 
 import LogoImage from "../../assets/ignite-logo.svg";
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={LogoImage} alt="" />
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
