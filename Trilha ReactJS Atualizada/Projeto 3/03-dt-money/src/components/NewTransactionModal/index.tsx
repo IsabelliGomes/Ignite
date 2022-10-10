e73a0e238736e6b0c61db1037007225349b57a91
Isabelli Gomes <isabelli.pgomes@poli.ufrj.br>
2022-10-10 08:36:05 -0300

    Projeto 3 - Performance: Utilizando React DevTools, Context Selectors, useCallback e useMemo

--- Trilha ReactJS Atualizada/Projeto 3/03-dt-money/src/components/NewTransactionModal/index.tsx
+++ Trilha ReactJS Atualizada/Projeto 3/03-dt-money/src/components/NewTransactionModal/index.tsx
@@ -11,7 +11,7 @@ import {
   TransactionType,
   TransactionTypeButton,
 } from './styles'
-import { useContext } from 'react'
+import { useContextSelector } from 'use-context-selector'
 import { TransactionsContext } from '../../contexts/TransactionsContext'
 
 const NewTransactionFormSchema = z.object({
@@ -24,7 +24,12 @@ const NewTransactionFormSchema = z.object({
 type NewTransactionFormInputs = z.infer<typeof NewTransactionFormSchema>
 
 export function NewTransactionModal() {
-  const { createTransaction } = useContext(TransactionsContext)
+  const createTransaction = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.createTransaction
+    },
+  )
 
   const {
     control,
