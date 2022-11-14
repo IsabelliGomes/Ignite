a927850609daaa228aba93e0db2c3f04112d546e
Isabelli Gomes <49256056+IsabelliGomes@users.noreply.github.com>
2022-11-14 18:47:09 -0300

    Projeto 4 - Produto & Checkout: Navegação com Lin e prefetch, SSG com parametro dinamico e uso do fallback, API routes, checkout com stripe, redirect ssr e basico de seo

--- Trilha ReactJS Atualizada/Projeto 4/04-ignite-shop/src/pages/index.tsx
+++ Trilha ReactJS Atualizada/Projeto 4/04-ignite-shop/src/pages/index.tsx
@@ -1,6 +1,10 @@
-import Image from "next/image";
 import { GetStaticProps } from "next";
-import { useKeenSlider } from "keen-slider/react";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+
+import { useKeenSlider } from "keen-slider/react"
+;
 import Stripe from "stripe";
 import { stripe } from "../lib/stripe";
 
@@ -12,7 +16,7 @@ interface HomeProps{
     id: string;
     name: string;
     imageUrl: string;
-    price: number;
+    price: string;
   }[]
 }
 
@@ -52,19 +56,32 @@ export default function Home({ products }: HomeProps) {
   })
 
   return (
-    <HomeContainer ref={sliderRef} className="keen-slider">
-      {products.map(product => {
-        return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
+    
+      <HomeContainer ref={sliderRef} className="keen-slider">      
+        {products.map(product => {
+          return (
+            <Link
+              href={`/product/${product.id}`} 
+              key={product.id}
+              prefetch={false}
+            >
+              <Product className="keen-slider__slide">
+                <Image src={product.imageUrl} width={520} height={480} alt="" />
 
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
-        )
-      })}
-    </HomeContainer>
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>
+            
+          )
+        })}
+      </HomeContainer>
+    </>
   )
 }
--- Trilha ReactJS Atualizada/Projeto 4/04-ignite-shop/src/pages/product/[id].tsx
+++ Trilha ReactJS Atualizada/Projeto 4/04-ignite-shop/src/pages/product/[id].tsx
@@ -1,9 +1,108 @@
-import { useRouter } from "next/router"
+import { GetStaticPaths, GetStaticProps } from "next"
+import axios from 'axios'
+import Image from "next/image";
+import { useRouter } from "next/router";
+import Stripe from "stripe";
+import { stripe } from "../../lib/stripe";
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
+import { useState } from "react";
+import Head from "next/head";
 
-export default function Product() {
-  const { query } = useRouter()
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    defaultPriceId: string;
+    price: string;
+    description: string;
+  }
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      { params: { id: 'prod_MnhP9Myg5WlPYR' } }, //produtos mais acessados / mais vendidos
+    ],
+    fallback: true, //'blocking' -> Não deixa a página ser carregada até que os dados sejam carregados
+  }
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+  const productId = params.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price'],
+  });
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        defaultPriceId: price.id,
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        }).format(price.unit_amount / 100),
+        description: product.description
+      }
+    },
+    revalidate: 60 * 60 * 1, // 1 hour
+  }
+}
+
+export default function Product({ product }: ProductProps) {
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false);
+
+  const { isFallback } = useRouter();
+  if (isFallback) {
+    return <p>Carregando...</p>
+  }
+
+  async function handleByProduct() {
+    try {
+      setIsCreatingCheckoutSession(true);
+
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId,
+      })
+
+      const { checkoutUrl } = response.data;
+
+      window.location.href = checkoutUrl;
+    } catch (error) {
+      setIsCreatingCheckoutSession(false);
+      // Conectar com o Sentry / Datalog
+      alert('Falha ao redicionar para o checkout')
+    }
+  }
 
   return (
-    <h1>Product</h1>
+    <>
+      <Head>
+        <title>{product.name} | Ignite Shop</title>
+      </Head>
+      
+      <ProductContainer>
+        <ImageContainer>
+          <Image src={product.imageUrl} width={520} height={480} alt=""/>
+        </ImageContainer>
+
+        <ProductDetails>
+          <h1>{product.name}</h1>
+          <span>{product.price}</span>
+
+          <p>{product.description}</p>
+
+          <button disabled={isCreatingCheckoutSession} onClick={handleByProduct}>
+            Comprar agora
+          </button>
+        </ProductDetails>
+      </ProductContainer>
+    </>
   )
 }
